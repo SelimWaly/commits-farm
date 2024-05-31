0f50395871ce1e0e7b962fd6871c04d71768f3a2
Selim Waly <selimwalyy@gmail.com>
2024-05-31 22:38:17 +0300

    Create command-line interaction
    Created command-line interaction instead of forcing users to alter code to work.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ account for no particular reason but
 clout, fame and the fact that it just
 looks cool.
 """
+
 import os
 import subprocess
 import random
@@ -12,9 +13,38 @@ import time
 import string
 from datetime import datetime
 
-github_username = "SelimWaly"
-repository_name = "commit"
-proj_dir = "D:/VSCode/GitHub Commits Farm"
+def get_valid_input(prompt):
+    while True:
+        user_input = input(prompt).strip()
+        if user_input:
+            return user_input
+        print("Input cannot be empty or whitespace. Please try again.")
+
+def get_project_directory():
+    while True:
+        proj_dir = input("Enter the directory to operate in: ").replace("/", "\\").strip()
+        if proj_dir:
+            if not os.path.exists(proj_dir):
+                try:
+                    os.makedirs(proj_dir)
+                    print(f"Directory '{proj_dir}' created.")
+                except OSError as e:
+                    print(f"Error creating directory: {e}")
+                    continue
+            return proj_dir
+        print("Input cannot be empty or whitespace. Please try again.")
+
+github_username = get_valid_input("Enter your GitHub username: ")
+user_name = get_valid_input("Enter your name: ")
+user_email = get_valid_input("Enter your email: ")
+
+# Set Git configuration
+subprocess.run(["git", "config", "--global", "user.name", user_name], check=True)
+subprocess.run(["git", "config", "--global", "user.email", user_email], check=True)
+
+repository_name = get_valid_input("Enter your GitHub repository name: ")
+proj_dir = get_project_directory()
+
 os.chdir(proj_dir)
 
 def generate_salt():
@@ -31,168 +61,91 @@ while True:
     generated = random.randint(1, 10)
 
     if generated == 1 or generated == 2 or generated == 3:
-        print("Awaiting to remove monitoring on suspicousness from GitHub servers...")
-        time.sleep(15)
-
-    elif generated == 4:
-        try:
-            print("Creating a commit with a C++ file...")
-            file_content = """#include <iostream>
-
-            int main() {
-                std::cout << "Hello, World!" << std::endl;
-                return 0;
-            }
-            """
-            print("Generating salt...")
-            salt = generate_salt()
-            file_path = os.path.join(proj_dir, f"Temp-{salt}.cpp")
-            with open(file_path, "w") as file:
-                file.write(file_content)
-            subprocess.run(["git", "init"])
-            subprocess.run(["git", "add", f"Temp-{salt}.cpp"])
-            subprocess.run(["git", "commit", "-m", f"Add Temp-{salt}.cpp"])
-            github_repo_url = f"https://github.com/{github_username}/{repository_name}.git"
-            subprocess.run(["git", "remote", "add", "origin", github_repo_url])
-            subprocess.run(["git", "push", "-u", "origin", "master"])
-            os.remove(file_path)
-            commits+=1
-            print(f"Commits: {commits}")
-        except Exception as e:
-            print(f"Reloading due to {e}")
-
-    elif generated == 5:
-        try:
-            print("Creating a commit with a C file...")
-            file_content = """#include <stdio.h>
-
-            int main() {
-                printf("Hello, world!\n");
-                return 0;
-            }
-
-            """
-            print("Generating salt...")
-            salt = generate_salt()
-            file_path = os.path.join(proj_dir, f"Temp-{salt}.c")
-            with open(file_path, "w") as file:
-                file.write(file_content)
-            subprocess.run(["git", "init"])
-            subprocess.run(["git", "add", f"Temp-{salt}.c"])
-            subprocess.run(["git", "commit", "-m", f"Add Temp-{salt}.c"])
-            github_repo_url = f"https://github.com/{github_username}/{repository_name}.git"
-            subprocess.run(["git", "remote", "add", "origin", github_repo_url])
-            subprocess.run(["git", "push", "-u", "origin", "master"])
-            os.remove(file_path)
-            commits+=1
-            print(f"Commits: {commits}")
-        except Exception as e:
-            print(f"Reloading due to {e}")
-
-    elif generated == 6:
-        try:
-            print("Creating a commit with a Python file...")
-            file_content = """print('Hello, world!') """
-            print("Generating salt...")
-            salt = generate_salt()
-            file_path = os.path.join(proj_dir, f"Temp-{salt}.py")
-            with open(file_path, "w") as file:
-                file.write(file_content)
-            subprocess.run(["git", "init"])
-            subprocess.run(["git", "add", f"Temp-{salt}.py"])
-            subprocess.run(["git", "commit", "-m", f"Add Temp-{salt}.py"])
-            github_repo_url = f"https://github.com/{github_username}/{repository_name}.git"
-            subprocess.run(["git", "remote", "add", "origin", github_repo_url])
-            subprocess.run(["git", "push", "-u", "origin", "master"])
-            os.remove(file_path)
-            commits+=1
-            print(f"Commits: {commits}")
-        except Exception as e:
-            print(f"Reloading due to {e}")
-
-    elif generated == 7:
-        try:
-            print("Creating a commit with a Jupyter file...")
-            file_content = """print('Hello, world!') """
-            print("Generating salt...")
-            salt = generate_salt()
-            file_path = os.path.join(proj_dir, f"Temp-{salt}.ipynb")
-            with open(file_path, "w") as file:
-                file.write(file_content)
-            subprocess.run(["git", "init"])
-            subprocess.run(["git", "add", f"Temp-{salt}.ipynb"])
-            subprocess.run(["git", "commit", "-m", f"Add Temp-{salt}.ipynb"])
-            github_repo_url = f"https://github.com/{github_username}/{repository_name}.git"
-            subprocess.run(["git", "remote", "add", "origin", github_repo_url])
-            subprocess.run(["git", "push", "-u", "origin", "master"])
-            os.remove(file_path)
-            commits+=1
-            print(f"Commits: {commits}")
-        except Exception as e:
-            print(f"Reloading due to {e}")
-
-    elif generated == 8:
-        try:
-            print("Creating a commit with a JavaScript file...")
-            file_content = """console.log('Hello, world!'); """
-            print("Generating salt...")
-            salt = generate_salt()
-            file_path = os.path.join(proj_dir, f"Temp-{salt}.js")
-            with open(file_path, "w") as file:
-                file.write(file_content)
-            subprocess.run(["git", "init"])
-            subprocess.run(["git", "add", f"Temp-{salt}.js"])
-            subprocess.run(["git", "commit", "-m", f"Add Temp-{salt}.js"])
-            github_repo_url = f"https://github.com/{github_username}/{repository_name}.git"
-            subprocess.run(["git", "remote", "add", "origin", github_repo_url])
-            subprocess.run(["git", "push", "-u", "origin", "master"])
-            os.remove(file_path)
-            commits+=1
-            print(f"Commits: {commits}")
-        except Exception as e:
-            print(f"Reloading due to {e}")
-
-    elif generated == 9:
-        try:
-            print("Creating a commit with a TypeScript file...")
-            file_content = """console.log('Hello, world!'); """
-            print("Generating salt...")
-            salt = generate_salt()
-            file_path = os.path.join(proj_dir, f"Temp-{salt}.ts")
-            with open(file_path, "w") as file:
-                file.write(file_content)
-            subprocess.run(["git", "init"])
-            subprocess.run(["git", "add", f"Temp-{salt}.ts"])
-            subprocess.run(["git", "commit", "-m", f"Add Temp-{salt}.ts"])
-            github_repo_url = f"https://github.com/{github_username}/{repository_name}.git"
-            subprocess.run(["git", "remote", "add", "origin", github_repo_url])
-            subprocess.run(["git", "push", "-u", "origin", "master"])
-            os.remove(file_path)
-            commits+=1
-            print(f"Commits: {commits}")
-        except Exception as e:
-            print(f"Reloading due to {e}")
+        print("Awaiting to remove monitoring on suspiciousness from GitHub servers...")
+        time.sleep(10)
 
     else:
         try:
-            print("Creating a commit with a Brainfuck file...")
-            file_content = """++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>++++.--------.++++++++++++.--------.--------.
-    """
+            file_content = ""
+            file_extension = ""
+            if generated == 4:
+                print("Creating a commit with a C++ file...")
+                file_content = """#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+"""
+                file_extension = "cpp"
+            elif generated == 5:
+                print("Creating a commit with a C file...")
+                file_content = """#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\\n");
+    return 0;
+}
+"""
+                file_extension = "c"
+            elif generated == 6:
+                print("Creating a commit with a Python file...")
+                file_content = """print('Hello, world!') """
+                file_extension = "py"
+            elif generated == 7:
+                print("Creating a commit with a Jupyter file...")
+                file_content = """print('Hello, world!') """
+                file_extension = "ipynb"
+            elif generated == 8:
+                print("Creating a commit with a JavaScript file...")
+                file_content = """console.log('Hello, world!'); """
+                file_extension = "js"
+            elif generated == 9:
+                print("Creating a commit with a TypeScript file...")
+                file_content = """console.log('Hello, world!'); """
+                file_extension = "ts"
+            else:
+                print("Creating a commit with a Brainfuck file...")
+                file_content = """++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++.>++++.--------.++++++++++++.--------.--------.
+"""
+                file_extension = "bf"
+
             print("Generating salt...")
             salt = generate_salt()
-            file_path = os.path.join(proj_dir, f"Temp-{salt}.bf")
+            file_path = os.path.join(proj_dir, f"Temp-{salt}.{file_extension}")
             with open(file_path, "w") as file:
                 file.write(file_content)
-            subprocess.run(["git", "init"])
-            subprocess.run(["git", "add", f"Temp-{salt}.bf"])
-            subprocess.run(["git", "commit", "-m", f"Add Temp-{salt}.bf"])
+
+            subprocess.run(["git", "init"], check=True)
+            subprocess.run(["git", "add", file_path], check=True)
+            subprocess.run(["git", "commit", "-m", f"Add Temp-{salt}.{file_extension}"], check=True)
+            
             github_repo_url = f"https://github.com/{github_username}/{repository_name}.git"
-            subprocess.run(["git", "remote", "add", "origin", github_repo_url])
-            subprocess.run(["git", "push", "-u", "origin", "master"])
+            
+            # Remove existing remote if it exists
+            subprocess.run(["git", "remote", "remove", "origin"], check=True, stderr=subprocess.DEVNULL)
+            
+            subprocess.run(["git", "remote", "add", "origin", github_repo_url], check=True)
+            
+            # Fetch the latest changes from the remote repository
+            subprocess.run(["git", "fetch", "origin"], check=True)
+            
+            # Stash any local changes
+            subprocess.run(["git", "stash"], check=True)
+            
+            # Rebase the local changes on top of the remote changes
+            subprocess.run(["git", "rebase", "origin/master"], check=True)
+            
+            # Apply the stashed changes, if any
+            subprocess.run(["git", "stash", "pop"], check=True, stderr=subprocess.DEVNULL)
+            
+            # Push the changes to the remote repository
+            subprocess.run(["git", "push", "-u", "origin", "master"], check=True)
+
             os.remove(file_path)
-            commits+=1
+            commits += 1
             print(f"Commits: {commits}")
-        except Exception as e:
+        except subprocess.CalledProcessError as e:
             print(f"Reloading due to {e}")
-    
+
     time.sleep(3)
